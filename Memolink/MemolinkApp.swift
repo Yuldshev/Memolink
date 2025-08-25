eb5d0cb6d52c44b58aa0b3171174af5241598f07
yuldshev <ikrom921@gmail.com>
2025-08-25 15:01:38 +0500

    Initial Commit

---
+++ Memolink/MemolinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemolinkApp.swift
+//  Memolink
+//
+//  Created by T on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MemolinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
